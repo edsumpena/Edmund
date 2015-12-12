61c7d2edd1836a355fd3124cc1e5b0c24c508ceb
joshuaj13579 <joshuaj13579@gmail.com>
2015-12-11 19:47:18 -0800

    Autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -3,48 +3,136 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.hardware.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.util.Range;
 
 public class Autonomous extends LinearOpMode {
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor leftfrontMotor;
+    DcMotor leftbackMotor;
+    DcMotor rightfrontMotor;
+    DcMotor rightbackMotor;
+    ModernRoboticsI2cGyro sensorGyro;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        leftMotor = hardwareMap.dcMotor.get("left_drive");
-        rightMotor = hardwareMap.dcMotor.get("right_drive");
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
-        leftMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        rightMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftfrontMotor =hardwareMap.dcMotor.get("leftfront_motor");
+        leftbackMotor =hardwareMap.dcMotor.get("leftback_motor");
+        rightfrontMotor = hardwareMap.dcMotor.get("rightfront_motor");
+        rightbackMotor = hardwareMap.dcMotor.get("rightback_motor");
+        rightfrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightbackMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftfrontMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        rightbackMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        sensorGyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        sensorGyro.calibrate();
 
         waitForStart();
-        leftMotor.setPower(1.0);
-        rightMotor.setPower(1.0);
-        sleep(250);
-        leftMotor.setPower(-1.0);
-        rightMotor.setPower(1.0);
-        sleep(800);
 
-        leftMotor.setPower(1.0);
-        rightMotor.setPower(1.0);
-        sleep(4250);
+        //move forward for 250 ms
+        leftfrontMotor.setPower(1.0);
+        leftbackMotor.setPower(1.0);
+        rightfrontMotor.setPower(1.0);
+        rightbackMotor.setPower(1.0);
+        sleep(50);
 
-        leftMotor.setPower(-1.0);
-        rightMotor.setPower(1.0);
-        sleep(1500);
-
-        leftMotor.setPower(1.0);
-        rightMotor.setPower(1.0);
+        //turn 45 degrees
+          turnAngle(-45.0);
+        //leftMotor.setPower(-1.0);
+        //rightMotor.setPower(1.0);
+        //sleep(850);
+        //move forward for 4250 ms
+        leftfrontMotor.setPower(1.0);
+        leftbackMotor.setPower(1.0);
+        rightfrontMotor.setPower(1.0);
+        rightbackMotor.setPower(1.0);
+        sleep(3250);
+        //telemetry.addData("forward", "");
+        //turn 90 degrees
+        turnAngle(-90.0);
+        //leftMotor.setPower(-1.0);
+        //rightMotor.setPower(1.0);
+        //sleep(900);
+        //move forward for 1000 ms
+        //moveDistance(3);
+        leftfrontMotor.setPower(1.0);
+        leftbackMotor.setPower(1.0);
+        rightfrontMotor.setPower(1.0);
+        rightbackMotor.setPower(1.0);
         sleep(1000);
+        //stop robot
+        leftfrontMotor.setPower(0.0);
+        leftbackMotor.setPower(0.0);
+        rightfrontMotor.setPower(0.0);
+        rightbackMotor.setPower(0.0);
+    }
+
+    private void turnAngle(double theta) throws InterruptedException {
+        leftfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        double e = theta;
+        double k = 1.0/90.0;
+        double a;
+        telemetry.addData("Heading", sensorGyro.getHeading());
+        sensorGyro.calibrate();
+        while(sensorGyro.isCalibrating());
+
+
+        while(e > 0.1 || e < -0.1){
+            a = sensorGyro.getHeading();
+            if (a > 180){
+                a = a - 360;
+            }
+            telemetry.addData("Heading", a);
+            e = theta - a;
+            telemetry.addData("error", e);
+            double u = e * k;
+            u = Range.clip(u,-1, 1);
+            telemetry.addData("error", e);
+            leftfrontMotor.setPower(u);
+            leftbackMotor.setPower(u);
+            rightfrontMotor.setPower(-u);
+            rightbackMotor.setPower(-u);
+        }
+        leftfrontMotor.setPower(0.0);
+        leftbackMotor.setPower(0.0);
+        rightfrontMotor.setPower(0.0);
+        rightbackMotor.setPower(0.0);
+
+
 
     }
 
-    private void turnAngle(double theta)
-    {
 
-    }
 
-    private void moveDistance(double distance)
-    {
+    /*private void moveDistance(double distance) throws InterruptedException {
 
-    }
+        leftMotor.setPower(1.0);
+        rightMotor.setPower(1.0);
+        sleep(distance);
+
+
+        leftMotor.setMode(DcMotorController.RunMode .RESET_ENCODERS);
+        rightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+        leftMotor.setTargetPosition((int)(distance*1440));
+        rightMotor.setTargetPosition((int)(distance*1440));
+
+        leftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+       rightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        leftMotor.setPower(1.0);
+        rightMotor.setPower(1.0);
+
+        while(leftMotor.isBusy()&& rightMotor.isBusy());
+
+        leftMotor.setPower(0.0);
+        rightMotor.setPower(0.0);
+
+    }*/
 }
