76c38227940db9c99d07b968c377efc91f25ef07
RobertYL <rob3rtyl33@gmail.com>
2015-12-13 15:16:37 -0800

    Tidied the code up

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -19,14 +19,18 @@ public class Autonomous extends LinearOpMode {
         leftbackMotor =hardwareMap.dcMotor.get("leftback_motor");
         rightfrontMotor = hardwareMap.dcMotor.get("rightfront_motor");
         rightbackMotor = hardwareMap.dcMotor.get("rightback_motor");
+
         rightfrontMotor.setDirection(DcMotor.Direction.REVERSE);
         rightbackMotor.setDirection(DcMotor.Direction.REVERSE);
+
         leftfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         leftbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
         leftfrontMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         rightbackMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
         sensorGyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
         sensorGyro.calibrate();
 
@@ -40,29 +44,27 @@ public class Autonomous extends LinearOpMode {
         sleep(50);
 
         //turn 45 degrees
-          turnAngle(-45.0);
-        //leftMotor.setPower(-1.0);
-        //rightMotor.setPower(1.0);
-        //sleep(850);
+        turnAngle(-45.0);
+
         //move forward for 4250 ms
         leftfrontMotor.setPower(1.0);
         leftbackMotor.setPower(1.0);
         rightfrontMotor.setPower(1.0);
         rightbackMotor.setPower(1.0);
         sleep(3250);
+
         //telemetry.addData("forward", "");
         //turn 90 degrees
         turnAngle(-90.0);
-        //leftMotor.setPower(-1.0);
-        //rightMotor.setPower(1.0);
-        //sleep(900);
+
         //move forward for 1000 ms
-        //moveDistance(3);
+
         leftfrontMotor.setPower(1.0);
         leftbackMotor.setPower(1.0);
         rightfrontMotor.setPower(1.0);
         rightbackMotor.setPower(1.0);
         sleep(1000);
+
         //stop robot
         leftfrontMotor.setPower(0.0);
         leftbackMotor.setPower(0.0);
@@ -75,9 +77,11 @@ public class Autonomous extends LinearOpMode {
         leftbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightfrontMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         rightbackMotor.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
         double e = theta;
         double k = 1.0/90.0;
         double a;
+
         telemetry.addData("Heading", sensorGyro.getHeading());
         sensorGyro.calibrate();
         while(sensorGyro.isCalibrating());
@@ -94,6 +98,7 @@ public class Autonomous extends LinearOpMode {
             double u = e * k;
             u = Range.clip(u,-1, 1);
             telemetry.addData("error", e);
+
             leftfrontMotor.setPower(u);
             leftbackMotor.setPower(u);
             rightfrontMotor.setPower(-u);
@@ -103,9 +108,7 @@ public class Autonomous extends LinearOpMode {
         leftbackMotor.setPower(0.0);
         rightfrontMotor.setPower(0.0);
         rightbackMotor.setPower(0.0);
-
-
-
+        
     }
 
 
